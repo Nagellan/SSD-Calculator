4306b0564735d48e072ec50b2abf594624603d6c
Nagellan <31626879+Nagellan@users.noreply.github.com>
2020-01-31 19:47:18 +0300

    Add input string validation

--- calc.js
+++ calc.js
@@ -1,10 +1,35 @@
 class Calculator {
   constructor() {
-    
+    this.parser = new Parser();
   }
 
-  solve(expr) {
-    console.log(expr);
+  solve(str) {
+    const expr = this.parser.parse(str);
+  }
+}
+
+class Parser {
+  constructor() {
+    this.validator = new Validator();
+  }
+
+  parse(str) {
+    str = this.validator.validateStr(str);
+  }
+}
+
+class Validator {
+  strIsValid(str) {
+    return /^\s*(\d+(?=\.)\.)?\d+\s*([+\-*/]\s*(\d+(?=\.)\.)?\d+\s*)*$/.test(str);
+  }
+
+  validateStr(str) {
+    if (!this.strIsValid(str)) throw new Error("Input string is not valid!");
+    return str;
+  }
+
+  validateExpr(expr) {
+    // TODO
   }
 }
 
